a7654fdb4b231321926f981479206af1bccd25ba
Pablo González Doval <pablo.gonzalez@masmovil.com>
2019-07-16 10:44:11 +0200

    Added creation of subscription if not exists

--- pubsub/src/main/java/com/masmovil/service/pubsub/PubSubServiceImpl.java
+++ pubsub/src/main/java/com/masmovil/service/pubsub/PubSubServiceImpl.java
@@ -1,20 +1,15 @@
 package com.masmovil.service.pubsub;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
 import com.google.cloud.pubsub.v1.TopicAdminClient;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.PushConfig;
 import com.masmovil.service.pubsub.exceptions.SubscriptionCreationException;
-
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.functions.Consumer;
@@ -22,9 +17,16 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.Vertx;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class PubSubServiceImpl implements PubSubService {
 
   private static final int TIMEOUT = 20;
+  public static final int ACK_DEADLINE_SECONDS = 180;
   private final ProjectTopicName topicName;
   private final ProjectSubscriptionName projectSubscriptionName;
 
@@ -35,6 +37,7 @@ public class PubSubServiceImpl implements PubSubService {
    * {@link PubSubService#fromEnv(String, String, String)},
    * {@link PubSubService#fromValues(String, String, String)} or
    * {@link PubSubServiceImpl#test()} (which is also package-private and intended only for testing)
+   *
    * @param projectId
    * @param topicId
    * @param subscriptionId
@@ -42,21 +45,10 @@ public class PubSubServiceImpl implements PubSubService {
   PubSubServiceImpl(String projectId, String topicId, String subscriptionId) {
     topicName = ProjectTopicName.of(projectId, topicId);
     projectSubscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId);
-    createTopicIfNotExists(topicId).subscribe();
+    createTopicIfNotExists(topicId)
+        .andThen(createSubscriptionIfNotExists(subscriptionId))
+        .subscribe();
 
-  }
-
-  private Completable createTopicIfNotExists(String topicId) {
-    return Completable.fromAction(() -> {
-      try (TopicAdminClient topicAdminClient = TopicAdminClient.create()) {
-        log.info("Attempting to create pubsub topic " + topicId);
-        topicAdminClient.createTopic(topicName);
-        log.info("Created topic " + topicId);
-      } catch(Exception e) {
-          log.info("Could not create topic " + topicId + ". Already exists?");
-          log.info(e.getMessage());
-      }
-    });
   }
 
   /**
@@ -66,9 +58,36 @@ public class PubSubServiceImpl implements PubSubService {
     return new PubSubServiceImpl(
         System.getenv("TEST_PUB_SUB_PROJECT_ID"),
         System.getenv("TEST_PUB_SUB_TOPIC_ID"),
-            System.getenv("TEST_PUB_SUB_SUBSCRIPTION_ID"));
+        System.getenv("TEST_PUB_SUB_SUBSCRIPTION_ID"));
   }
 
+  private Completable createTopicIfNotExists(String topicId) {
+    return Completable.fromAction(() -> {
+      try (TopicAdminClient topicAdminClient = TopicAdminClient.create()) {
+        log.info("Attempting to create pubsub topic " + topicId);
+        topicAdminClient.createTopic(topicName);
+        log.info("Created topic " + topicId);
+      } catch (Exception e) {
+        log.info("Could not create topic " + topicId + ". Already exists?");
+        log.info(e.getMessage());
+      }
+    });
+  }
+
+  private Completable createSubscriptionIfNotExists(String subscriptionId) {
+    return Completable.fromAction(() -> {
+      try (SubscriptionAdminClient subscriptionAdminClient = SubscriptionAdminClient.create()) {
+        log.info("Attempting to create pubsub subscription " + subscriptionId);
+        PushConfig pushConfig = PushConfig.newBuilder().build();
+        subscriptionAdminClient.createSubscription(projectSubscriptionName, topicName, pushConfig
+            , ACK_DEADLINE_SECONDS);
+        log.info("Created subscription " + subscriptionId);
+      } catch (Exception e) {
+        log.info("Could not create subscription " + subscriptionId + ". Already exists?");
+        log.info(e.getMessage());
+      }
+    });
+  }
 
   /**
    * Package-private method for ack-ing all previous messages, intended for setting up the
@@ -77,15 +96,15 @@ public class PubSubServiceImpl implements PubSubService {
   <T> Completable ackAllPrevious(T hay, Class<T> tClass, Vertx vertx) {
     List<PubSubSubscriberImpl<T>> subscribers = new ArrayList<>(1);
     return Completable.create(completableEmitter ->
-        {
-          subscribers.add(addSubscriber(t -> {
-            //System.out.println("acking " + t);
-            if (t.equals(hay)) {
-              completableEmitter.onComplete();
-            }
-          }, tClass, vertx));
-          publish(hay).subscribe();
-        }).andThen(Completable.fromAction(() -> subscribers
+    {
+      subscribers.add(addSubscriber(t -> {
+        //System.out.println("acking " + t);
+        if (t.equals(hay)) {
+          completableEmitter.onComplete();
+        }
+      }, tClass, vertx));
+      publish(hay).subscribe();
+    }).andThen(Completable.fromAction(() -> subscribers
         .forEach(PubSubSubscriberImpl::dispose)))
         .andThen(Completable.fromAction(subscribers::clear));
   }
@@ -111,7 +130,8 @@ public class PubSubServiceImpl implements PubSubService {
   }
 
   @Override
-  public <T> PubSubSubscriberImpl<T> addSubscriber(Consumer<T> consumer, Class<T> tClass, Vertx vertx) throws SubscriptionCreationException {
+  public <T> PubSubSubscriberImpl<T> addSubscriber(Consumer<T> consumer, Class<T> tClass,
+      Vertx vertx) throws SubscriptionCreationException {
     try {
       PubSubSubscriberImpl<T> subscriber = new PubSubSubscriberImpl<>(vertx, tClass, projectSubscriptionName, consumer);
       return subscriber;
